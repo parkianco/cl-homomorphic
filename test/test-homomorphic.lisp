0218689c2426223d27528f83faa85e86a8671f6e
Park Ian Co <opensource@parkian.co>
2026-03-13 20:44:00 -0400

    Update copyright headers

---
+++ test/test-homomorphic.lisp
@@ -0,0 +1,22 @@
+;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
+;; SPDX-License-Identifier: BSD-3-Clause
+
+;;;; test-homomorphic.lisp - Unit tests for homomorphic
+;;;;
+;;;; Copyright (c) 2024-2026 Parkian Company LLC. All rights reserved.
+;;;; SPDX-License-Identifier: BSD-3-Clause
+
+(defpackage #:cl-homomorphic.test
+  (:use #:cl)
+  (:export #:run-tests))
+
+(in-package #:cl-homomorphic.test)
+
+(defun run-tests ()
+  "Run all tests for cl-homomorphic."
+  (format t "~&Running tests for cl-homomorphic...~%")
+  ;; TODO: Add test cases
+  ;; (test-function-1)
+  ;; (test-function-2)
+  (format t "~&All tests passed!~%")
+  t)
